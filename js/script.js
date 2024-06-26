4a0fd3bf89c6568a58492f83158f23d44be59cc0
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-06-26 09:59:33 +0200

    [UPD] Added condition for age < 18

--- js/script.js
+++ js/script.js
@@ -6,4 +6,18 @@ let eta = prompt('Inserisci la tua età');
 
 // Realizzo una variabile per contenere il prezzo base
 let prezzo_base = km * 0.21;
-console.log(prezzo_base);
+
+// Dichiaro una variabile che mi conterrà il prezzo finale
+let prezzo_finale;
+
+// SE l'età è inferiore a 18 => applico uno sconto del 20%
+if (eta < 18) {
+    // Calcolo a partire dal prezzo base lo sconto
+    // let sconto = prezzo_base * 20 / 100;
+    let sconto = prezzo_base * 0.2;
+    // calcolo il prezzo finale
+    prezzo_finale = prezzo_base - sconto;
+    // prezzo_finale = prezzo_base * 0.8;
+}
+
+console.log(prezzo_finale)
