b30ab12b753d5dfd3c4acc92eb3c040f5d444447
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-06-26 09:51:33 +0200

    [UPD] Calculated base price

--- js/script.js
+++ js/script.js
@@ -4,3 +4,6 @@ let km = prompt('Inserisci i km da percorrere');
 // Realizzare una variabile alla quale assegno un prompt per chiedere l'età da percorrere
 let eta = prompt('Inserisci la tua età');
 
+// Realizzo una variabile per contenere il prezzo base
+let prezzo_base = km * 0.21;
+console.log(prezzo_base);
