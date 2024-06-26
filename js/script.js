911a4e087f4b073b2d09499bb9934ca39a1f1992
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-06-26 09:48:48 +0200

    [UPD] Variables done

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// Realizzare una variabile alla quale assegno un prompt per chiedere i km da percorrere
+let km = prompt('Inserisci i km da percorrere');
+
+// Realizzare una variabile alla quale assegno un prompt per chiedere l'età da percorrere
+let eta = prompt('Inserisci la tua età');
+
